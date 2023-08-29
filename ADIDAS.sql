c698d9f066674d3b23ffbdc4db186896a015b0a8
cheryl1411 <141177899+cheryl1411@users.noreply.github.com>
2023-08-29 17:06:59 +0100

    Add files via upload

--- ADIDAS.sql
+++ ADIDAS.sql
@@ -8,7 +8,7 @@ SELECT DISTINCT product FROM sales.adidas_us; -- nice to plot
 SELECT DISTINCT sales_method FROM sales.adidas_us; -- nice to plot
 SELECT DISTINCT retailer FROM sales.adidas_us; -- nice to plot
 
--- ANALYZE:
+-- QUESTION:
 -- 1.	What's the total revenue and profitability of the dataset? (entire dataset, 2020 and 2021)
 
 SELECT
@@ -22,14 +22,14 @@ FROM
     sales.adidas_us;
     
 -- 2. Which states drive the most revenue and have high profitability?
-
 SELECT state, 
 	SUM(total_sales) AS revenue, 
 	SUM(operating_profit) AS profit
 FROM sales.adidas_us
 GROUP BY state
-ORDER BY revenue;
-    -- •	Are there any states that consistently show higher operating margins? 
+ORDER BY revenue DESC;
+
+    -- 3. Are there any states that consistently show higher operating margins? 
 SELECT
     y2020.state,
     y2020.avg_margin_2020,
@@ -51,7 +51,7 @@ ON y2020.state = y2021.state
 WHERE (y2021.avg_margin_2021 - y2020.avg_margin_2020) > 0;
 
 
--- 3. Are certain sales methods consistently more profitable?
+-- 4. Are certain sales methods consistently more profitable?
 -- •	Overall sales by selling method
 SELECT sales_method,
 	SUM(total_sales) AS revenue,
@@ -76,7 +76,7 @@ SELECT
     SQRT((COUNT(*) * SUM(units_sold * units_sold) - SUM(units_sold) * SUM(units_sold)) *
          (COUNT(*) * SUM(operating_profit * operating_profit) - SUM(operating_profit) * SUM(operating_profit))) AS correlation_coefficient
 FROM sales.adidas_us;
--- 8.	Do revenue and profitability follow seasonal trends? (2021, 2022) -- Use for visual time trend
+-- 6.	Do revenue and profitability follow seasonal trends? (2021, 2022) -- Use for visual time trend
 SELECT
     EXTRACT(MONTH FROM STR_TO_DATE(invoice_date, '%d/%m/%Y')) AS month,
     SUM(total_sales) AS revenue,
@@ -94,4 +94,33 @@ FROM sales.adidas_us
 WHERE EXTRACT(YEAR FROM STR_TO_DATE(invoice_date, '%d/%m/%Y')) = 2021
 GROUP BY month
 ORDER BY revenue DESC;
+
+-- 7. Product
+-- Top selling product and highest profitability entire dataset 
+SELECT product,
+    SUM(CASE WHEN EXTRACT(YEAR FROM STR_TO_DATE(invoice_date, '%d/%m/%Y')) = 2020 THEN total_sales END) AS revenue_2020,
+    SUM(CASE WHEN EXTRACT(YEAR FROM STR_TO_DATE(invoice_date, '%d/%m/%Y')) = 2020 THEN units_sold END) AS total_units_2020,
+    SUM(CASE WHEN EXTRACT(YEAR FROM STR_TO_DATE(invoice_date, '%d/%m/%Y')) = 2021 THEN total_sales END) AS revenue_2021,
+    SUM(CASE WHEN EXTRACT(YEAR FROM STR_TO_DATE(invoice_date, '%d/%m/%Y')) = 2021 THEN units_sold END) AS total_units_2021,
+    SUM(total_sales) as total_revenue,
+    SUM(operating_profit) AS total_profit,
+    AVG(operating_margin) AS avg_margin
+FROM sales.adidas_us
+GROUP BY product
+ORDER BY 6,7 DESC;
+
+-- AVG Price
+SELECT
+    product,
+    AVG(CASE WHEN EXTRACT(YEAR FROM STR_TO_DATE(invoice_date, '%d/%m/%Y')) = 2020 THEN price_per_unit END) AS avg_price_2020,
+    AVG(CASE WHEN EXTRACT(YEAR FROM STR_TO_DATE(invoice_date, '%d/%m/%Y')) = 2021 THEN price_per_unit END) AS avg_price_2021,
+    AVG(price_per_unit) AS avg_price
+FROM sales.adidas_us
+GROUP BY product;
+
+
+
+
+
+
 
